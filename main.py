52ec02a3c64d422821ca53350dbd22b6cba5a4ba
invinciblejai <mailjai.vardhan@gmail.com>
2019-03-08 19:20:50 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# one way to impprt
+import module
+
+# another way to import 
+from module import func1
+
+# based on first import
+print(module.func1('import1'))
+
+# based on second import
+print(func1('import2'))
+
